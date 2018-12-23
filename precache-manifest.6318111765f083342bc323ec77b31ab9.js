9dc8d961573f90cf0a6ff0a15c5a2f29148feffb
Deepak Sharma S M <smdeepaksharma@gmail.com>
2018-12-22 19:30:00 -0800

    Updates

---
+++ precache-manifest.6318111765f083342bc323ec77b31ab9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "34a6c7dfe6a7bba2c695",
+    "url": "/portfolio/static/js/main.34a6c7df.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/portfolio/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "34a6c7dfe6a7bba2c695",
+    "url": "/portfolio/static/css/main.7931b3de.chunk.css"
+  },
+  {
+    "revision": "144441ed9571bb2d187adfb83e22d44d",
+    "url": "/portfolio/index.html"
+  }
+];
